ccfd1d7dfc5b10b857420a8119f49c9362a58072
Darya-Kazharnovich <darya.kazharnovich@gmail.com>
2022-05-22 21:52:08 +0300

    add 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
-
+const fs = require('fs')
+const path = require('path')
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+    if (err) {
+        console.error(err)
+    }
+})
+fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, items) => {
+    if (err) {
+        console.error(err)
+    } else {
+        for (let i = 0; i<items.length; i++) {
+            const fileName = items[i].name
+            const pathToFile = path.join(__dirname, 'files', `${fileName}`)
+            const pathToFileClone = path.join(__dirname, 'files-copy', `${fileName}`)
+            fs.copyFile(`${pathToFile}`, `${pathToFileClone}`, (err) => {
+                if (err) {
+                    console.error(err)
+                }
+            })
+        }
+    }
+})
