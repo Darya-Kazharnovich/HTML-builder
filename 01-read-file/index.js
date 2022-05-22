7526df31b6e3c012879e6ec3cb8444f986606c83
Darya-Kazharnovich <darya.kazharnovich@gmail.com>
2022-05-22 20:55:40 +0300

    add 02 and 03

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const route = path.join(__dirname, 'text.txt')
-const readStream = fs.createReadStream(route)
-readStream.on('data', (data) => {
-    console.log(data.toString())
+const readStream = fs.createReadStream(route, 'utf-8')
+readStream.on('data', data => {
+    console.log(data)
 })
